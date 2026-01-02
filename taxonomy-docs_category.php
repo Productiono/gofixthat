8c5d48bdcc002204dd348b1cb6748a00365be60e
taskpul <romtietz@gmail.com>
2026-01-02 06:20:03 +0100

    Improve docs header consistency and search behavior

--- taxonomy-docs_category.php
+++ taxonomy-docs_category.php
@@ -27,6 +27,18 @@ add_filter(
 	}
 );
 
+add_action(
+	'wp_head',
+	function() {
+		?>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+		<?php
+	},
+	1
+);
+
 add_action( 'wp_head', 'mbf_docs_render_header_styles' );
 add_action(
 	'wp_head',
